0f548b704152a0970aed12bcb2fb3f524cbdb363
Akshay Chawla <chawla.akshay1234@gmail.com>
2017-10-05 17:20:12 +0530

    ImageNet FGSM method for many images (Works!) the output is dumped to a .pkl file
    <TODO>: Create a new visualizer for mnist .pkl from ImageNet FGSM.

--- imagenet-fast-gradient.py
+++ imagenet-fast-gradient.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-import cv2 
+from PIL import Image
+import torch
 from torch.autograd import Variable
 import torch.nn as nn 
 import torch.nn.functional as F 
@@ -9,27 +10,71 @@ import torch.optim as optim
 from torchvision import transforms
 from tqdm import *
 import torchvision.models 
-from PIL import Image
+import pickle
 
 with open("./labels.json","r") as f: 
     import json 
     ImageNet_mapping = json.loads(f.read())
 
+def image_location_generator(_root):
+    import os
+    _dirs = os.listdir(_root)
+    assert len(_dirs) > 0, "no directories in given root folder"
+    for _dir in _dirs:
+        _imfiles = os.listdir(os.path.join(_root,_dir))
+        for _imfile in _imfiles:
+            yield os.path.join(_root, _dir, _imfile)
+
 imsize = (224, 224)
 loader = transforms.Compose([transforms.Scale(imsize), transforms.ToTensor()])
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 def image_loader(image_name):
-    """load image, returns cuda tensor"""
+    """load image, returns tensor"""
     image = Image.open(image_name)
+    image = image.convert("RGB") # Auto remove the "alpha" channel from png image
     image = loader(image).float()
     image = normalize(image).float()
-    image = Variable(image, requires_grad=True)
     image = image.unsqueeze(0)  #this is for VGG, may not be needed for ResNet
     return image 
-image = image_loader("./cat.jpeg")
+
+# Pretrained VGG16 model
 vgg16 = torchvision.models.vgg16(pretrained=True)
-output = vgg16.forward(image)
-output = output.data.numpy() 
-print ImageNet_mapping[str(output.argmax())], np.argmax(output)
+vgg16.eval() # disable dropout, batchnorm
+SoftmaxWithXent = nn.CrossEntropyLoss()
+print ".. loaded pre-trained vgg16"
+
+images, labels, y_preds, noise, adversarial_class = [], [], [], [], []
+for _imloc in image_location_generator("./downloads/"):
+    
+    x = Variable(image_loader(_imloc), requires_grad=True)
+    images.append(x.data.numpy())
+    output = vgg16.forward(x)
+    y_preds.append(ImageNet_mapping[str(output.data.numpy().argmax())])
+    labels.append(ImageNet_mapping[str(output.data.numpy().argmax())])
+    y = Variable(torch.LongTensor(np.array([output.data.numpy().argmax()])), requires_grad = False)
+    loss = SoftmaxWithXent(output, y)
+    loss.backward()
+
+    # Add perturbation 
+    epsilon = 0.1
+    x_g     = torch.sign(x.grad.data)
+    adv_x   = torch.clamp(x.data + epsilon*x_g, 0, 255) 
+
+    # Check classification 
+    adversarial_class.append(ImageNet_mapping[str(np.argmax(vgg16.forward(Variable(adv_x)).data.numpy()))])
+    noise.append((adv_x - x.data).numpy())
+
+    # Display 
+    print y_preds[-1], " | ", adversarial_class[-1]
+
+with open("adv_results_imagenet_fgsm.pkl", "w") as f: 
+   adv_data_dict = {
+           'x' : images, 
+           'y_true': labels, 
+           'y_pred': y_preds,
+           'r': noise,
+           'adversarial_class': adversarial_class 
+           }
+    pickle.dump(adv_data_dict, f)
 
 
